b6bc3ef8645d56f79798527596a9cb3ebd352846
Siddhant Sharma <siddhant.sharma.21cse@bmu.edu.in>
2024-04-08 23:13:37 +0530

    Added 2 components of home

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/navbar";
 import How from "../components/how";
 import Footer from "../components/footer";
 import Hero from "../components/hero";
+import Feedback from "../components/Feedback";
 function Homepage(){
     return(
         <>
         <Navbar/>
         <Hero/>
         <How/>
+        <Feedback/>
         <Footer/>
         </>
     )
